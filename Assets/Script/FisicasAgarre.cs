f502b223e6971a8001ac1d314c9392dc5c323e7a
MiguelDiuza <132395122+MiguelDiuza@users.noreply.github.com>
2024-05-31 18:02:17 -0500

    objetos

--- Assets/Script/FisicasAgarre.cs
+++ Assets/Script/FisicasAgarre.cs
@@ -6,37 +6,52 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class FisicasAgarre : MonoBehaviour
 {
     private XRGrabInteractable grabInteractable;
+    private Vector3 originalPosition;
+    private Quaternion originalRotation;
     private Transform originalParent;
+
+    public float returnSpeed = 1.0f; // Velocidad a la que el objeto regresa a su posición original
 
     // Start is called before the first frame update
     void Start()
     {
         grabInteractable = GetComponent<XRGrabInteractable>();
-        originalParent = GetComponent<Transform>();
+        originalParent = transform.parent;
+        originalPosition = transform.localPosition;
+        originalRotation = transform.localRotation;
+
         grabInteractable.onSelectExited.AddListener(OnRelease);
-        
     }
 
     private void OnRelease(XRBaseInteractor interactor)
     {
-        if (originalParent != null)
-        {
-            transform.SetParent(originalParent);
-            transform.localPosition = Vector3.zero;
-            transform.localRotation = Quaternion.identity;
-            transform.localScale = Vector3.one;
+        StartCoroutine(ReturnToOriginalPosition());
+    }
 
-        }
-        else
+    private IEnumerator ReturnToOriginalPosition()
+    {
+        yield return new WaitForEndOfFrame(); // Espera un frame para asegurar que la física se haya aplicado
+
+        float elapsedTime = 0.0f;
+        Vector3 startPosition = transform.localPosition;
+        Quaternion startRotation = transform.localRotation;
+
+        while (elapsedTime < 1.0f)
         {
-            Debug.LogWarning("El objeto no tinene padre original");
+            transform.localPosition = Vector3.Lerp(startPosition, originalPosition, elapsedTime * returnSpeed);
+            transform.localRotation = Quaternion.Lerp(startRotation, originalRotation, elapsedTime * returnSpeed);
+            elapsedTime += Time.deltaTime;
+            yield return null;
         }
 
+        transform.localPosition = originalPosition;
+        transform.localRotation = originalRotation;
+        transform.SetParent(originalParent);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
- }
+}
